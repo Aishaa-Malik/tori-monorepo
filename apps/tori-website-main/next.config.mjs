feb5a378e7b6fe705c9c48367467b4de4195af0a
Aishaa-Malik <aisha.faang@gmail.com>
2026-02-13 15:08:39 +0530

    added wrkspaces in root pjson

--- apps/tori-website-main/next.config.mjs
+++ apps/tori-website-main/next.config.mjs
@@ -3,13 +3,26 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/services/:path*',
-        destination: 'https://tori-dashboard.vercel.app/:path*', 
+        source: '/login',
+        destination: 'https://tori-dashboard.vercel.app/login',
       },
       {
         source: '/dashboard/:path*',
-        destination: 'https://tori-dashboard.vercel.app/:path*',
+        destination: 'https://tori-dashboard.vercel.app/dashboard/:path*',
       },
+      {
+        source: '/onboarding',
+        destination: 'https://tori-dashboard.vercel.app/onboarding',
+      },
+      {
+        source: '/healthwellness-dashboard/:path*',
+        destination: 'https://tori-dashboard.vercel.app/healthwellness-dashboard/:path*',
+      },
+      // Keep your services directory if it's also in the dashboard
+      {
+        source: '/services/:path*',
+        destination: 'https://tori-dashboard.vercel.app/services/:path*',
+      }
     ]
   },
 };
