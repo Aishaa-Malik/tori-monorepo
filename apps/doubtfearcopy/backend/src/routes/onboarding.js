6893992f2f0efac8052ff06e827b957b8593679b
Aishaa-Malik <aisha.faang@gmail.com>
2026-06-01 17:27:07 +0530

    public-listings removed from onboarding.js

--- apps/doubtfearcopy/backend/src/routes/onboarding.js
+++ apps/doubtfearcopy/backend/src/routes/onboarding.js
@@ -6,8 +6,8 @@ const onboardingController = require('../controllers/onboardingController');
 router.get('/check-onboarding', onboardingController.checkOnboardingStatus);
 
 // Public service listings
-router.get('/public-service-listings', onboardingController.getPublicServiceListings);
-router.get('/public-service-listings/:serviceId', onboardingController.getPublicServiceListingById);
+// router.get('/public-service-listings', onboardingController.getPublicServiceListings);
+// router.get('/public-service-listings/:serviceId', onboardingController.getPublicServiceListingById);
 
 // Save onboarding data
 router.post('/save-onboarding', onboardingController.saveOnboardingData);
